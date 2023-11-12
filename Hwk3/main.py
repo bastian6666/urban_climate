62a3179b01cf83cddc9e6d7eab2d71893720424e
bastian6666 <57109930+bastian6666@users.noreply.github.com>
2023-11-11 22:59:45 -0500

    End of hwk 3

--- Hwk3/main.py
+++ Hwk3/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+sns.set_palette("Set2")
+
 
 def convert_units(df, site):
     df[f'Ta_{site}'] = df[f'Ta_{site}'] + 273.15
@@ -19,9 +23,9 @@ df_grass = convert_units(df_grass, 'grass')
 df_urban = convert_units(df_urban, '26_5m')
 
 # Save the dataframes
-# df_crop.to_csv('data_clean/diurnal_crop_converted.csv', index=False)
-# df_grass.to_csv('data_clean/diurnal_grass_converted.csv', index=False)
-# df_urban.to_csv('data_clean/diurnal_urban_converted.csv', index=False)
+df_crop.to_csv('data_clean/diurnal_crop_converted.csv', index=False)
+df_grass.to_csv('data_clean/diurnal_grass_converted.csv', index=False)
+df_urban.to_csv('data_clean/diurnal_urban_converted.csv', index=False)
 
 
 def calculate_humidity(df, site):
@@ -78,25 +82,39 @@ plot_diurnal_cycles(df_crop, df_grass, df_urban, 'e_star')
 plot_diurnal_cycles(df_crop, df_grass, df_urban, 'RH')
 
 
-def compare_RH(df, site):
-    plt.figure(figsize=(6, 6))
 
-    # Assuming 'RH_given' is the column name for the given relative humidity
-    plt.scatter(df[f'Rh_{site}'], df[f'RH_{site}'])
+def compare_RH(df1, site1, df2, site2, df3, site3):
+    fig, axs = plt.subplots(1, 3, figsize=(12, 8))
 
-    # Draw the line y = x
-    limits = [min(plt.xlim()[0], plt.ylim()[0]), max(plt.xlim()[1], plt.ylim()[1])]
-    plt.plot(limits, limits, color='red')
+    # Subplot 1: Compare RH for site1
+    axs[0].scatter(df1[f'Rh_{site1}'], df1[f'RH_{site1}'])
+    limits = [min(axs[0].get_xlim()[0], axs[0].get_ylim()[0]), max(axs[0].get_xlim()[1], axs[0].get_ylim()[1])]
+    axs[0].plot(limits, limits, color='red')
+    axs[0].set_title(f'Comparison of Given and Computed RH at {site1}', fontsize=14)
+    axs[0].set_xlabel('Given RH (%)', fontsize=12)
+    axs[0].set_ylabel('Computed RH (%)', fontsize=12)
 
-    plt.title(f'Comparison of Given and Computed RH at {site}')
-    plt.xlabel('Given RH (%)')
-    plt.ylabel('Computed RH (%)')
+    # Subplot 2: Compare RH for site2
+    axs[1].scatter(df2[f'Rh_{site2}'], df2[f'RH_{site2}'])
+    limits = [min(axs[1].get_xlim()[0], axs[1].get_ylim()[0]), max(axs[1].get_xlim()[1], axs[1].get_ylim()[1])]
+    axs[1].plot(limits, limits, color='red')
+    axs[1].set_title(f'Comparison of Given and Computed RH at {site2}', fontsize=14)
+    axs[1].set_xlabel('Given RH (%)', fontsize=12)
+    axs[1].set_ylabel('Computed RH (%)', fontsize=12)
+
+    # Subplot 3: Compare RH for site3
+    axs[2].scatter(df3[f'Rh_{site3}'], df3[f'RH_{site3}'])
+    limits = [min(axs[2].get_xlim()[0], axs[2].get_ylim()[0]), max(axs[2].get_xlim()[1], axs[2].get_ylim()[1])]
+    axs[2].plot(limits, limits, color='red')
+    axs[2].set_title(f'Comparison of Given and Computed RH at {site3}', fontsize=14)
+    axs[2].set_xlabel('Given RH (%)', fontsize=12)
+    axs[2].set_ylabel('Computed RH (%)', fontsize=12)
+
+    plt.tight_layout(pad=3.0)
     plt.show()
 
 # Compare RH for each dataframe
-compare_RH(df_crop, 'crop')
-compare_RH(df_grass, 'grass')
-compare_RH(df_urban, '26_5m')
+compare_RH(df_crop, 'crop', df_grass, 'grass', df_urban, '26_5m')
 
 def plot_diurnal_cycles(df, site):
     # Assuming there's a 'hour' column in your dataframes
@@ -125,7 +143,7 @@ plot_diurnal_cycles(df_urban, '26_5m')
 
 
 def calculate_heat_index(df, site):
-    T = df[f'Ta_{site}']
+    T = (df[f'Ta_{site}'] - 273.15) * 9/5 + 32
     R = df[f'Rh_{site}']
 
     # Compute the heat index
@@ -144,19 +162,20 @@ df_grass.to_csv('data_clean/diurnal_grass_converted.csv', index=False)
 df_urban.to_csv('data_clean/diurnal_urban_converted.csv', index=False)
 
 
+
 def plot_diurnal_cycles_heat_index(df_crop, df_grass, df_urban):
 
     plt.figure(figsize=(10, 6))
 
     # Plot heat index for all three sites
-    plt.plot(df_crop['heat_index_crop'], label='Heat Index (crop)', linewidth=2)
-    plt.plot(df_grass['heat_index_grass'], label='Heat Index (grass)', linewidth=2)
-    plt.plot(df_urban['heat_index_26_5m'], label='Heat Index (urban)', linewidth=2)
+    sns.lineplot(data=df_crop['heat_index_crop'], label='Heat Index (crop)', linewidth=2)
+    sns.lineplot(data=df_grass['heat_index_grass'], label='Heat Index (grass)', linewidth=2)
+    sns.lineplot(data=df_urban['heat_index_26_5m'], label='Heat Index (urban)', linewidth=2)
 
     plt.legend(fontsize=12)
     plt.title('Average Diurnal Cycles of Heat Index', fontsize=16)
     plt.xlabel('Hour of the day', fontsize=14)
-    plt.ylabel('Heat Index (°C)', fontsize=14)
+    plt.ylabel('Heat Index (°F)', fontsize=14)
 
     # Add gridlines
     plt.grid(True)
@@ -165,3 +184,128 @@ def plot_diurnal_cycles_heat_index(df_crop, df_grass, df_urban):
 
 # Plot diurnal cycles for all dataframes
 plot_diurnal_cycles_heat_index(df_crop, df_grass, df_urban)
+
+
+def calculate_partial_derivatives(df, site):
+    # Calculate d1, d2, and d3
+    df[f'd1_{site}'] = -df[f'e_{site}'] / (df[f'e_star_{site}']**2)
+    print(df[f'd1_{site}'])
+    df[f'd2_{site}'] = (2.50398e6*np.exp((17.27*df[f'Ta_{site}']-4717.3)/(df[f'Ta_{site}']-35.85)))/(df[f'Ta_{site}']-35.85)**2
+    print(df[f'd2_{site}'])
+    df[f'd3_{site}'] = 1/(df[f'e_star_{site}'])
+
+    return df
+
+# Calculate function value for each dataframe
+df_crop = calculate_partial_derivatives(df_crop, 'crop')
+df_grass = calculate_partial_derivatives(df_grass, 'grass')
+df_urban = calculate_partial_derivatives(df_urban, '26_5m')
+
+df_crop.to_csv('data_clean/diurnal_crop_converted.csv', index=False)
+df_grass.to_csv('data_clean/diurnal_grass_converted.csv', index=False)
+df_urban.to_csv('data_clean/diurnal_urban_converted.csv', index=False)
+
+def calculate_terms(df_crop, df_urban, site):
+    df_crop[f'term1_{site}'] = 100*(df_crop[f'd1_{site}'] * df_crop[f'd2_{site}'] * (-df_crop[f'Ta_{site}'] + df_urban['Ta_26_5m']))
+    df_crop[f'term2_{site}'] = 100*(df_crop[f'd3_{site}'] * (-df_crop[f'e_{site}'] + df_urban['e_26_5m']))
+
+    df_crop[f'DeltaRH_terms_{site}'] = df_crop[f'term1_{site}'] + df_crop[f'term2_{site}']
+
+                                          
+    return df_crop
+
+# Calculate product for each dataframe
+df_crop = calculate_terms(df_crop, df_urban, 'crop')
+df_crop.to_csv('data_clean/diurnal_crop_converted.csv', index=False)
+
+
+
+def calculate_delta_RH(df1, df2, site1, site2):
+    df1[f'delta_RH_{site1}_{site2}'] =  df2[f'RH_{site2}'] - df1[f'RH_{site1}']
+    return df1
+
+# Calculate ∆RH for crop and urban
+df_crop = calculate_delta_RH(df_crop, df_urban, 'crop', '26_5m')
+
+# Calculate ∆RH for grass and urban
+df_grass = calculate_delta_RH(df_grass, df_urban, 'grass', '26_5m')
+
+# Save the dataframes
+df_crop.to_csv('data_clean/diurnal_crop_converted.csv', index=False)
+df_grass.to_csv('data_clean/diurnal_grass_converted.csv', index=False)
+
+
+def plot_subfigures(df, site1, site2):
+    fig, axs = plt.subplots(1, 2, figsize=(24, 8))
+
+    # Subplot 1: Term1, Term2 and their Sum
+    axs[0].plot(df[f'term1_{site1}'], label=f'term1_{site1}', linewidth=2)
+    axs[0].plot(df[f'term2_{site1}'], label=f'term2_{site1}', linewidth=2)
+    axs[0].plot(df[f'term1_{site1}'] + df[f'term2_{site1}'], label='sum', linestyle='--', linewidth=2)
+    axs[0].set_xlabel('Hour', fontsize=14)
+    axs[0].set_ylabel('RH_urban-RH_crop (%)', fontsize=14)
+    axs[0].set_title(f'Term1, Term2 and their Sum for {site1}', fontsize=16)
+    axs[0].legend(fontsize=12)
+
+    # Subplot 2: Comparison of DeltaRH_terms and delta_RH
+    axs[1].plot(df[f'DeltaRH_terms_{site1}'], label=f'DeltaRH_terms_{site1}', linewidth=2)
+    axs[1].plot(df[f'delta_RH_{site1}_{site2}'], label=f'delta_RH_{site1}_{site2}', linewidth=2)
+    axs[1].set_xlabel('Hour', fontsize=14)
+    axs[1].set_ylabel('RH_urban-RH_crop (%)', fontsize=14)
+    axs[1].set_title(f'Comparison of DeltaRH_terms_{site1} and delta_RH_{site1}_{site2}', fontsize=16)
+    axs[1].legend(fontsize=12)
+
+    plt.subplots_adjust(wspace=0.3)
+    plt.savefig('e.4_e.5.pdf', format='pdf')
+    plt.show()
+
+# Plot subfigures for crop and urban
+plot_subfigures(df_crop, 'crop', '26_5m')
+
+
+def compute_densities(df, site):
+    R_dry_air = 287.058  # specific gas constant for dry air in J/(kg·K)
+    R_water_vapor = 461.5  # specific gas constant for water vapor in J/(kg·K)
+
+
+    # Compute air density
+    df[f'air_density_{site}'] = df[f'P_mean_{site}'] / (R_dry_air * df[f'Ta_{site}'])
+
+    # Compute water vapor density
+    df[f'water_vapor_density_{site}'] = df[f'P_mean_{site}'] / (R_water_vapor * df[f'Ta_{site}'])
+
+    return df
+
+def plot_diurnal_cycles(df_crop, df_grass, df_urban, site1, site2, site3):
+    fig, axs = plt.subplots(1, 2, figsize=(12, 8))
+
+    # Plot air density for all three sites
+    sns.lineplot(data=df_crop[f'air_density_{site1}'], label=f'Air Density ({site1})', linewidth=2, ax=axs[0])
+    sns.lineplot(data=df_grass[f'air_density_{site2}'], label=f'Air Density ({site2})', linewidth=2, ax=axs[0])
+    sns.lineplot(data=df_urban[f'air_density_{site3}'], label=f'Air Density ({site3})', linewidth=2, ax=axs[0])
+
+    axs[0].legend(fontsize=12)
+    axs[0].set_title('Average Diurnal Cycles of Air Densities', fontsize=16)
+    axs[0].set_xlabel('Hour of the day', fontsize=14)
+    axs[0].set_ylabel('Air Density (kg/m³)', fontsize=14)
+    axs[0].grid(True)
+
+    # Plot water vapor density for all three sites
+    sns.lineplot(data=df_crop[f'water_vapor_density_{site1}'], label=f'Water Vapor Density ({site1})', linewidth=2, ax=axs[1])
+    sns.lineplot(data=df_grass[f'water_vapor_density_{site2}'], label=f'Water Vapor Density ({site2})', linewidth=2, ax=axs[1])
+    sns.lineplot(data=df_urban[f'water_vapor_density_{site3}'], label=f'Water Vapor Density ({site3})', linewidth=2, ax=axs[1])
+
+    axs[1].legend(fontsize=12)
+    axs[1].set_title('Average Diurnal Cycles of Water Vapor Densities', fontsize=16)
+    axs[1].set_xlabel('Hour of the day', fontsize=14)
+    axs[1].set_ylabel('Water Vapor Density (kg/m³)', fontsize=14)
+    axs[1].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+df_crop = compute_densities(df_crop, 'crop')
+df_grass = compute_densities(df_grass, 'grass')
+df_urban = compute_densities(df_urban, '26_5m')
+
+plot_diurnal_cycles(df_crop, df_grass, df_urban, 'crop', 'grass', '26_5m')
